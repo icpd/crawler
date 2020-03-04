2a0dc6d08408734d5822d1033d0b896986d81b38
whoisix <35096485+whoisix@users.noreply.github.com>
2020-03-04 11:21:01 +0800

    添加验证

--- api/clash.go
+++ api/clash.go
@@ -13,11 +13,23 @@ import (
 func Clash(c *gin.Context) {
 	query := c.Request.URL.String()
 	idx := strings.Index(query, "sub_link=")
+
+	if idx < 0 {
+		c.String(http.StatusBadRequest, "sub_link=订阅链接.")
+		c.Abort()
+		return
+	}
+
 	val := query[idx+9:]
+	if val == "" {
+		c.String(http.StatusBadRequest, "sub_link 不能为空")
+		c.Abort()
+		return
+	}
 
 	content, err := subscribe.GetSubContent(val)
 	if err != nil {
-		c.String(http.StatusBadRequest, err.Error())
+		c.String(http.StatusBadRequest, "请求失败:"+err.Error())
 		c.Abort()
 		return
 	}
