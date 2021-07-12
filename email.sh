bb943c89abed053033e35c4a08de4dc377ed3fbd
whoisix <35096485+whoisix@users.noreply.github.com>
2021-07-12 17:38:48 +0800

    chore:email.sh

---
+++ email.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="xx@gmail.com"
+CORRECT_NAME="whoisix"
+CORRECT_EMAIL="35096485+whoisix@users.noreply.github.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
